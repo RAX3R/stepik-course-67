227d19a3b6cb12951b80c007f58ad097274dfeb0
RAX3R <raxer93@mail.ru>
2020-10-09 23:32:36 +0300

    2.6 - optimisation 4

--- 2 - Циклы. Строки. Условия./2.6 - Задачи по материалам недели/4.py
+++ 2 - Циклы. Строки. Условия./2.6 - Задачи по материалам недели/4.py
@@ -21,7 +21,7 @@ if str_input != stop_word:
     # Многомерный цикл:
     for i, ii in enumerate(matrix):
         for j, jj in enumerate(ii):
-            
+
             len_str = len(matrix)
             len_col = len(ii)
             m_str_end = len_str - 1
@@ -32,119 +32,100 @@ if str_input != stop_word:
                 matrix_copy[i][j] = str(int(matrix[i][j]) * 4)
 
             # Квадратная или произвольная матрица:
-            elif (len_str == len_col) or (len_str != len_col and len_str, len_col != 1):
+            elif (len_str == len_col) or (len_str != len_col and len_str != 1 and len_col != 1):
 
-                # 1:
-                if i == 0 and j == 0:
+                # First:
+                if i == 0:
                     up = int(matrix[m_str_end][j])
                     down = int(matrix[i + 1][j])
-                    left = int(matrix[i][m_col_end])
-                    right = int(matrix[i][j + 1])
+
+                    if j == 0:
+                        left = int(matrix[i][m_col_end])
+                        right = int(matrix[i][j + 1])
+
+                    elif 0 < j < m_col_end:
+                        left = int(matrix[i][j - 1])
+                        right = int(matrix[i][j + 1])
+
+                    elif j == m_col_end:
+                        left = int(matrix[i][j - 1])
+                        right = int(matrix[i][0])
+
                     matrix_copy[i][j] = str(up + down + left + right)
 
-                # 2 - середина:
-                elif i == 0 and 0 < j < m_col_end:
-                    up = int(matrix[m_str_end][j])
-                    down = int(matrix[i + 1][j])
-                    left = int(matrix[i][j - 1])
-                    right = int(matrix[i][j + 1])
-                    matrix_copy[i][j] = str(up + down + left + right)
-
-                # 3:
-                elif i == 0 and j == m_col_end:
-                    up = int(matrix[m_str_end][j])
-                    down = int(matrix[i + 1][j])
-                    left = int(matrix[i][j - 1])
-                    right = int(matrix[i][0])
-                    matrix_copy[i][j] = str(up + down + left + right)
-
-                # 4 - середина:
-                elif 0 < i < m_str_end and j == 0:
+                # Mid:
+                elif 0 < i < m_str_end:
                     up = int(matrix[i - 1][j])
                     down = int(matrix[i + 1][j])
-                    left = int(matrix[i][m_col_end])
-                    right = int(matrix[i][j + 1])
+
+                    if j == 0:
+                        left = int(matrix[i][m_col_end])
+                        right = int(matrix[i][j + 1])
+
+                    elif j < m_col_end:
+                        left = int(matrix[i][j - 1])
+                        right = int(matrix[i][j + 1])
+
+                    elif j == m_col_end:
+                        left = int(matrix[i][j - 1])
+                        right = int(matrix[i][0])
+
                     matrix_copy[i][j] = str(up + down + left + right)
 
-                # 5 - центральные:
-                elif 0 < i < m_str_end and 0 < j < m_col_end:
-                    up = int(matrix[i - 1][j])
-                    down = int(matrix[i + 1][j])
-                    left = int(matrix[i][j - 1])
-                    right = int(matrix[i][j + 1])
-                    matrix_copy[i][j] = str(up + down + left + right)
-
-                # 6 - середина:
-                elif 0 < i < m_str_end and j == m_col_end:
-                    up = int(matrix[i - 1][j])
-                    down = int(matrix[i + 1][j])
-                    left = int(matrix[i][j - 1])
-                    right = int(matrix[i][0])
-                    matrix_copy[i][j] = str(up + down + left + right)
-
-                # 7:
-                elif i == m_str_end and j == 0:
+                # Last:
+                elif i == m_str_end:
                     up = int(matrix[i - 1][j])
                     down = int(matrix[0][j])
-                    left = int(matrix[i][m_col_end])
-                    right = int(matrix[i][j + 1])
-                    matrix_copy[i][j] = str(up + down + left + right)
 
-                # 8:
-                elif i == m_str_end and 0 < j < m_col_end:
-                    up = int(matrix[i - 1][j])
-                    down = int(matrix[0][j])
-                    left = int(matrix[i][j - 1])
-                    right = int(matrix[i][j + 1])
-                    matrix_copy[i][j] = str(up + down + left + right)
+                    if j == 0:
+                        left = int(matrix[i][m_col_end])
+                        right = int(matrix[i][j + 1])
 
-                # 9:
-                elif i == m_str_end and j == m_col_end:
-                    up = int(matrix[i - 1][j])
-                    down = int(matrix[0][j])
-                    left = int(matrix[i][j - 1])
-                    right = int(matrix[i][0])
+                    elif 0 < j < m_col_end:
+                        left = int(matrix[i][j - 1])
+                        right = int(matrix[i][j + 1])
+
+                    elif j == m_col_end:
+                        left = int(matrix[i][j - 1])
+                        right = int(matrix[i][0])
+
                     matrix_copy[i][j] = str(up + down + left + right)
 
             # Матрица со столбцом = 1:
             elif len_str > 1 and len_col == 1:
+                left_right = int(matrix[i][j]) * 2
+
                 if i == 0:
                     up = int(matrix[m_str_end][j])
                     down = int(matrix[i + 1][j])
-                    left_right = int(matrix[i][j]) * 2
-                    matrix_copy[i][j] = str(up + down + left_right)
 
                 elif i == m_str_end:
                     up = int(matrix[i - 1][j])
                     down = int(matrix[0][j])
-                    left_right = int(matrix[i][j]) * 2
-                    matrix_copy[i][j] = str(up + down + left_right)
 
                 elif 0 < i < m_str_end:
                     up = int(matrix[i - 1][j])
                     down = int(matrix[i + 1][j])
-                    left_right = int(matrix[i][j]) * 2
-                    matrix_copy[i][j] = str(up + down + left_right)
+
+                matrix_copy[i][j] = str(up + down + left_right)
 
             # Матрица со строчкой = 1:
             elif len_str == 1 and len_col > 1:
+                up_down = int(matrix[i][j]) * 2
+
                 if j == 0:
-                    up_down = int(matrix[i][j]) * 2
                     left = int(matrix[i][m_col_end])
-                    right = int(matrix[i][j+1])
-                    matrix_copy[i][j] = str(up_down + left + right)
+                    right = int(matrix[i][j + 1])
 
                 elif j == m_col_end:
-                    up_down = int(matrix[i][j]) * 2
                     left = int(matrix[i][j - 1])
                     right = int(matrix[i][0])
-                    matrix_copy[i][j] = str(up_down + left + right)
 
                 elif 0 < j < m_col_end:
-                    up_down = int(matrix[i][j]) * 2
                     left = int(matrix[i][j - 1])
                     right = int(matrix[i][j + 1])
-                    matrix_copy[i][j] = str(up_down + left + right)
+
+                matrix_copy[i][j] = str(up_down + left + right)
             else:
                 break
 
